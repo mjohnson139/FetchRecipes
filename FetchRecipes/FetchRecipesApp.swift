738b0159f64ce62067d6a191b38f23d668d6ef3c
Matt Johnson <mjohnson139@gmail.com>
2024-03-22 10:50:19 -0400

    Initial Commit

---
+++ FetchRecipes/FetchRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipesApp.swift
+//  FetchRecipes
+//
+//  Created by Matthew Johnson on 3/22/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
