07d81841e07471dbfa44f4a4789b123bb615b80a
Matt Johnson <mjohnson139@gmail.com>
2024-03-25 16:01:26 -0400

    Adding a destination.

--- FetchRecipes/ViewModels.swift
+++ FetchRecipes/ViewModels.swift
@@ -2,10 +2,15 @@ import Foundation
 import SwiftUI
 
 class RecipeListModel: ObservableObject {
+  @Published var destination: Destination?
   let listName: String
   @Published var recipes: [Meal]
+  
+  enum Destination {
+    case detail(Meal)
+  }
 
-  init(listName: String = "Recipe List", recipes: [Meal]) {
+  init(destination: Destination? = .none, listName: String = "Recipe List", recipes: [Meal]) {
     self.listName = listName
     self.recipes = recipes
   }
