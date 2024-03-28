785dee8f6b443d128da110d98329b3b05b3f90c3
Matt Johnson <mjohnson139@gmail.com>
2024-03-27 20:27:32 -0400

    Moved endpoint urls to an enum.

--- FetchRecipes/ApiClient.swift
+++ FetchRecipes/ApiClient.swift
@@ -12,6 +12,11 @@ enum ApiError: LocalizedError {
   }
 }
 
+enum ApiEndpoints {
+  static let getList = URL(string: "https://themealdb.com/api/json/v1/1/filter.php?c=Dessert")!
+  static let getMealById: @Sendable (String) -> URL? = { URL(string: "https://themealdb.com/api/json/v1/1/lookup.php?i=\($0)") }
+}
+
 struct ApiClient {
   var getList: @Sendable () async throws -> [Meal]
   var getMealById: @Sendable (String) async throws -> Meal?
@@ -19,12 +24,12 @@ struct ApiClient {
 
 extension ApiClient {
   static let live = Self.init {
-    let model = try await requestModel(url: URL(string: "https://themealdb.com/api/json/v1/1/filter.php?c=Dessert")!)
+    let model = try await requestModel(url: ApiEndpoints.getList)
     return model.meals.sorted { $0.strMeal < $1.strMeal }
 
   } getMealById: { id in
-    guard let url = URL(string: "https://themealdb.com/api/json/v1/1/lookup.php?i=\(id)") else {
-      throw (ApiError.dataLoadError)
+    guard let url = ApiEndpoints.getMealById(id) else {
+      throw ApiError.dataLoadError
     }
 
     let model = try await requestModel(url: url)
@@ -36,7 +41,7 @@ extension ApiClient {
     guard let httpResponse = response as? HTTPURLResponse,
           httpResponse.statusCode == 200
     else {
-      throw (ApiError.dataLoadError)
+      throw ApiError.dataLoadError
     }
 
     let decoder = JSONDecoder()
